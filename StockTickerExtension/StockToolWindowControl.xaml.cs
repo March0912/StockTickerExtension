f5f2d4b5d7541cd9f17a7eb8336ef0d1fb52b12c
xzy <xuzeyun@jimuyida.com>
2025-11-06 15:07:26 +0800

    优化代码

--- StockTickerExtension/StockToolWindowControl.xaml.cs
+++ StockTickerExtension/StockToolWindowControl.xaml.cs
@@ -717,6 +717,10 @@ namespace StockTickerExtension
                         return CodeTextBox.Items.IndexOf(item);
                     }
                 }
+                if(item.ToString() == text)
+                {
+                    return CodeTextBox.Items.IndexOf(item);
+                }
             }
             return -1;
         }
@@ -1172,7 +1176,7 @@ namespace StockTickerExtension
                     }
                 }
 
-                if (string.IsNullOrEmpty(StatusText.Text))
+                if (string.IsNullOrEmpty(StatusText.Text) || StatusText.Text.Contains("Conitoring started"))
                 {
                     UpdateStatus($"Monitoring {snap.Code} {snap.Name}", System.Windows.Media.Brushes.Green);
                 }
