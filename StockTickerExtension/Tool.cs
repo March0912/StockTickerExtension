d1fd1f0758227d24a2bba1c920544f680b10859c
xzy <xuzeyun@jimuyida.com>
2026-06-18 11:18:29 +0800

    切换股的时候，自动获取该股的板块题材信息

--- StockTickerExtension/Tool.cs
+++ StockTickerExtension/Tool.cs
@@ -239,7 +239,7 @@ namespace StockTickerExtension
 
             if (stockType == StockMarket.StockA)
             {
-                TimeSpan morningStart = new TimeSpan(9, 30, 0);
+                TimeSpan morningStart = new TimeSpan(9, 15, 0);
                 TimeSpan morningEnd = new TimeSpan(11, 30, 0);
                 TimeSpan afternoonStart = new TimeSpan(13, 0, 0);
                 TimeSpan afternoonEnd = new TimeSpan(15, 0, 0);
